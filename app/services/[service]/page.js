6d309e7b0b1259ad4ed35a467f98361ebbc5aafe
Dip Singha <dipsingha2@gmail.com>
2023-08-26 02:37:55 +0600

    dynamic routing page created

--- app/services/[service]/page.js
+++ app/services/[service]/page.js
@@ -3,7 +3,34 @@ import React from "react";
 import { serviceData } from "../page";
 
 function Service({ params }) {
-  return <div className="h-screen mt-28"></div>;
+  console.log(params);
+  console.log(serviceData);
+  return (
+    <div className=" mt-28">
+      {serviceData.map((data) => (
+        <div
+          key={params.service}
+          className="flex flex-col justify-center items-center"
+        >
+          <h1 className="text-center text-yellow-500 lg:text-6xl text-4xl font-medium">
+            {params.service == data.id && data.serviceTitle}
+          </h1>
+          <p className="py-5 text-xl">
+            {params.service == data.id && data.serviceTag}
+          </p>
+          <img
+            src={params.service == data.id && data.serviceImg}
+            alt=""
+            className="img w-[800px] h-[400px] p-3 rounded-3xl object-cover"
+          />
+
+          <p className="py-5 text-center">
+            {params.service == data.id && data.serviceSubtitle}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Service;
